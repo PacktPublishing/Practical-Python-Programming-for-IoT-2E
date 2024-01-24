e6b83943809289b006d9716f550ae69f26d06cd9
garyns <gary@smart-itc.com.au>
2024-01-24 15:20:03 +1100

    Updates

--- chapter08/pico/transistor_pico.py
+++ chapter08/pico/transistor_pico.py
@@ -14,15 +14,30 @@ p = Pin(GP, Pin.OUT)
 # Note that after wrapping a Pin with PWM(), Pin methods including on(), off(), high(), low() and p.value(n) do not work.
 # We can use pwm.duty_u16(65535) for fully 'on' and pwm.duty_u16(0) for fully'off'
 pwm = PWM(p)
-pwm.freq(8000)                                                          # (1)
+pwm.freq(8000)
+
+# Note:                                                                 # (1)
+#   There is no equivalent PWM range mapping function in            
+#   machine.PWM that matches the functionality of 
+#   PiGPIO pi.set_PWM_range(GPIO, <range>). Instead we
+#   manually create the range mapping using the formula
+#   duty_cycle = int(65535 / 100 * duty_cycle_pc)
 
 try:
     print("On")
-    pwm.duty_u16(65535) # Fully On.
+    pwm.duty_u16(65535) # Fully On (max duty cycle).                    # (2)
+    # Note: We cannot use p.on(), p.value(1), etc 
+    # once a Pin has been wrapped in PWM(). That's
+    # why we set duty cycle to the maximum.
+    
     sleep(2)
 
     print("Off")
-    pwm.duty_u16(0) # Fully Off.
+    pwm.duty_u16(0) # Fully Off (0 duty cycle).
+    # Note: We cannot use p.off(), p.value(0), etc 
+    # once a Pin has been wrapped in PWM(). That's
+    # why we set duty cycle to the 0.
+
     sleep(2)
 
     # Fade In.
@@ -31,7 +46,7 @@ try:
     for duty_cycle_pc in range(0, 101):                                 # (3)
         print("Duty cycle {}%".format(duty_cycle_pc))
         
-        # Translate Duty Cycle % (0 - 100) into a argument value for duty_u16 (0 - 65535)
+        # Map Duty Cycle % (0 - 100) into a argument value for duty_u16 (0 - 65535)
         duty_cycle = int(65535 / 100 * duty_cycle_pc)
         
         pwm.duty_u16(duty_cycle)
@@ -44,7 +59,7 @@ try:
     for duty_cycle_pc in range(100, -1, -1):                             # (4)
         print("Duty cycle {}%".format(duty_cycle_pc))
         
-        # Translate Duty Cycle % (0 - 100) into a argument value for duty_u16 (0 - 65535)
+        # Map Duty Cycle % (0 - 100) into a argument value for duty_u16 (0 - 65535)
         duty_cycle = int(65535 / 100 * duty_cycle_pc)
         
         pwm.duty_u16(duty_cycle)
