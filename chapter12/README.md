46bca4aafdae9e32fa6f209758f70be199bae89d
garyns <gary@smart-itc.com.au>
2024-01-31 20:42:11 +1100

    Updates

---
+++ chapter12/README.md
@@ -0,0 +1,25 @@
+# Practical Python Programming for IoT 2nd Edition
+
+## Chapter 12 - Measuring Distance and Detecting Movement
+
+### Source Code
+
+* `rpi` folder - Raspberry Pi Python Code
+
+  * `requirements.txt` - Python dependencies required for this chapter
+  * `hc-sr04_rpi.py` - Distance Measurement with the HC-SR04 Ultrasonic Sensor
+  * `hc-sr501_rpi.py` - Movement Detection with the HC-SR501 PIR Sensor
+  * `hall_effect_digital_rpi.py` - Hall-Effect Sensor Example - Switch or Latching Type
+  * `hall_effect_analog_rpi.py` - Hall-Effect Sensor Example - Ratiometric Type
+
+* `pico` folder - Pico MicroPython Code
+
+@TODO
+
+### Datasheets
+
+None
+
+### Post Publication Updates and Errata
+
+None
---
+++ chapter12/rpi/hall_effect_analog_rpi.py
@@ -0,0 +1,55 @@
+"""
+File: chapter11/hall_effect_analog.py
+
+Hall-Effect Sensor Example - Ratiometric Type
+Tested with AH3505 Hall-Effect Sensor
+
+Dependencies:
+  pip3 install pigpio adafruit-circuitpython-ads1x15
+
+Built and tested with Python 3.7 on Raspberry Pi 4 Model B
+"""
+from time import sleep
+import pigpio
+
+# Below imports are part of Circuit Python and Blinka
+import board                                                      
+import busio
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+
+pi = pigpio.pi()
+
+# Create the I2C bus & ADS object.
+i2c = busio.I2C(board.SCL, board.SDA)
+ads = ADS.ADS1115(i2c)
+analog_channel_A0 = AnalogIn(ads, ADS.P0)  # ADS.P0 --> A0 
+
+if __name__ == '__main__':
+      
+    try:
+        # Sample a resting voltage to calculate delta voltage.
+        print("Calibrating... make sure magnet is not near hall effect sensor")
+
+        resting_volts = 0
+        sum_volts = 0
+
+        for n in range(0, 100):
+            # Take 100 samples
+            sum_volts = sum_volts + analog_channel_A0.voltage
+
+        resting_volts = sum_volts / 100
+
+        while True:
+            volts = analog_channel_A0.voltage
+            volts_delta = volts - resting_volts
+
+            # Analog input voltage will fluctuate a little. Using 1 decimal point
+            # to steady display output.
+            output = "Volts={:>5.1f}, Delta Volts={:>5.1f}".format(volts, volts_delta)
+            print(output)
+            sleep(0.05)
+
+    except KeyboardInterrupt:
+        i2c.deinit()                                                          
+        pi.stop()
