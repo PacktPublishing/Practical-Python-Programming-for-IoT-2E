d517f699284fd622801bde9f754646845c7db63f
garyns <gary@smart-itc.com.au>
2024-02-12 16:41:59 +1100

    Updates

--- chapter10/pico/pico_adc/ldr_calibration_config_pico.py
+++ chapter10/pico/pico_adc/ldr_calibration_config_pico.py
@@ -1,5 +1,5 @@
 # This file was automatically created by ldr_calibrate_pico.py
 # Number of samples: 100
-# Voltage range is 2.8592
-MIN_VOLTS = 0.1199
-MAX_VOLTS = 2.9791
+# Voltage range is 2.9017
+MIN_VOLTS = 0.1332
+MAX_VOLTS = 3.0349
--- chapter10/pico/pico_adc/moisture_calibration_config_pico.py
+++ chapter10/pico/pico_adc/moisture_calibration_config_pico.py
@@ -1,4 +1,5 @@
 # This file was automatically created by moisture_calibrate_pico.py
 # Number of samples: 100
-MIN_VOLTS = 0.0477
-MAX_VOLTS = 0.0476
+# Voltage range is -1.6226
+MIN_VOLTS = 1.8169
+MAX_VOLTS = 0.1944
