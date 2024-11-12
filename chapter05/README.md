8a71bd6df8885c4d3a0256a874ca371e40c22d2f
garyns <gary@smart-itc.com.au>
2024-11-12 17:41:43 +1100

    Updateswsq

--- chapter05/README.md
+++ chapter05/README.md
@@ -1,15 +1,31 @@
-# Practical Python Programming for IoT
+# Practical Python Programming for IoT 2nd Edition
 
-## Chapter 4 - Distributed Systems with Python and MQTT
+## Chapter 5 - Distributed Systems with Python and MQTT
 
-* `requirements.txt` - Python dependencies required for this chapter
+* `breadboard_layout.fzz` - Fritzing breadboard layouts circuits for Raspberry Pi &amp; Pico
+* `breadboard_layout.png` - Breadboard layouts circuits for Raspberry Pi &amp; Pico
 
-* `mqtt_led.py` - Paho MQTT Pub/Sub example to control a LED
+* `rpi` folder - Raspberry Pi Python Code
 
-* `mosquitto_pyiot.conf` - Mosquitto MQTT Broker configuration file to enable Web Sockets and Web Server
+@TODO FIXME
+  * `requirements.txt` - Python dependencies required for this chapter
+  * `mqtt_led.py` - Paho MQTT Pub/Sub example to control a LED
+  * `mosquitto_pyiot.conf` - Mosquitto MQTT Broker configuration file to enable Web Sockets and Web Server
+  * `mosquitto_www` - Folder containing HTML and JavaScript files for MQTT Web Page client
+  * `mosquitto_www/index.html` - MQTT Web Page Client to control LED
+  * `mosquitto_www/*.js` - JavaScript dependencies as used in index.html
 
-* `mosquitto_www` - Folder containing HTML and JavaScript files for MQTT Web Page client
+* `pico` folder - Pico MicroPython Code
+@TODO FIXME
 
-* `mosquitto_www/index.html` - MQTT Web Page Client to control LED
+### Datasheets
 
-* `mosquitto_www/*.js` - JavaScript dependencies as used in index.html
+None
+
+### Post Publication Updates and Errata
+
+None
+
+### Breadboard Layouts
+
+![Breadboard Layouts](./breadboard_layout.png)
--- chapter05/rpi/mqtt_led_rpi.py
+++ chapter05/rpi/mqtt_led_rpi.py
@@ -1,58 +1,59 @@
 """
-File: chapter04/mqtt_led.py
+chapter05/rpi/mqtt_led_rpi.py
 
 A full life-cycle Python + MQTT program to control an LED.
 
 Dependencies:
-  pip3 install paho-mqtt gpiozero pigpio
+  pip3 install paho-mqtt pigpio
 
-Built and tested with Python 3.7 on Raspberry Pi 4 Model B
+Built and tested with Python 3.11.22 on Raspberry Pi 5
 """
 import logging
 import signal
 import sys
 import json
-from time import sleep
-from gpiozero import Device, PWMLED
-from gpiozero.pins.pigpio import PiGPIOFactory
+import pigpio
 import paho.mqtt.client as mqtt                                                                # (1)
-
 
 # Initialize Logging
 logging.basicConfig(level=logging.WARNING)  # Global logging configuration
 logger = logging.getLogger("main")  # Logger for this module
 logger.setLevel(logging.INFO) # Debugging for this file.
 
-
-# Initialize GPIO
-Device.pin_factory = PiGPIOFactory() # Set GPIOZero to use PiGPIO by default.
-
-
 # Global Variables
-LED_GPIO_PIN = 21
+LED_GPIO = 21
 BROKER_HOST = "localhost"                                                                       # (2)
 BROKER_PORT = 1883
-CLIENT_ID = "LEDClient"                                                                         # (3)
+CLIENT_ID = "LEDClient-rPI"                                                                     # (3)
 TOPIC = "led"                                                                                   # (4)
 client = None  # MQTT client instance. See init_mqtt()                                          # (5)
-led = None     # PWMLED Instance. See init_led()
+
+# Initialize GPIO
+pi = pigpio.pi() 
+
+# 8000 max hardware timed frequency by default pigpiod configuration.
+pi.set_PWM_frequency(LED_GPIO, 8000)
+
+# We set the range to 0..100 to mimic 0%..100%. This means
+# calls to pi.set_PWM_dutycycle(GPIO_PIN, duty_cycle) now
+# take a value in the range 0 to 100 as the duty_cycle
+# parameter rather than the default range of 0..255.
+pi.set_PWM_range(LED_GPIO, 100)
+
+# Initialise LED brightness. Our PWM range is 0..100,
+# therefore our brightness level % maps directly.
+pi.set_PWM_dutycycle(LED_GPIO, 0) # Start with 0 dutycycle (LED off)
 
 
 """
 GPIO Related Functions
 """
-def init_led():
-    """Create and initialise an LED Object"""
-    global led
-    led = PWMLED(LED_GPIO_PIN)
-    led.off()
 
-
-def set_led_level(data):                                                                       # (6)
+def set_led_brightness(data):                                                                  # (6)  @FIXME fn renamed, fix in book
     """Set LED State to one of On, Blink or Off (Default)
       'data' expected to be a dictionary with the following format:
       {
-          "level": a number between 0 and 100,
+          "level": a number between 0 and 100
       }
     """
 
@@ -64,12 +65,12 @@ def set_led_level(data):                                                        
         if isinstance(level, int) or isinstance(level, float) or level.isdigit():
             # State is a number
             level = max(0, min(100, int(level))) # Bound state to range 0..100
-            led.value = level / 100  # Scale 0..100% back to 0..1
+            pi.set_PWM_dutycycle(LED_GPIO, level)
             logger.info("LED at brightness {}%".format(level))
 
         else:
             logger.info("Request for unknown LED level of '{}'. We'll turn it Off instead.".format(level))
-            led.value = 0 # 0% = Led off.
+            pi.set_PWM_dutycycle(LED_GPIO, 0) # 0% = Led off.
     else:
         logger.info("Message '{}' did not contain property 'level'.".format(data))
 
@@ -77,46 +78,42 @@ def set_led_level(data):                                                        
 """
 MQTT Related Functions and Callbacks
 """
-def on_connect(client, user_data, flags, connection_result_code):                              # (7)
+def on_connect(client, userdata, flags, reason_code, properties):                              # (7)
     """on_connect is called when our program connects to the MQTT Broker.
        Always subscribe to topics in an on_connect() callback.
        This way if a connection is lost, the automatic
        re-connection will also results in the re-subscription occurring."""
 
-    if connection_result_code == 0:                                                            # (8)
+    if reason_code == 0:                                                                       # (8)
         # 0 = successful connection
         logger.info("Connected to MQTT Broker")
     else:
         # connack_string() gives us a user friendly string for a connection code.
-        logger.error("Failed to connect to MQTT Broker: " + mqtt.connack_string(connection_result_code))
+        logger.error("Failed to connect to MQTT Broker: " + mqtt.connack_string(reason_code))
 
     # Subscribe to the topic for LED level changes.
     client.subscribe(TOPIC, qos=2)                                                             # (9)
 
 
 
-def on_disconnect(client, user_data, disconnection_result_code):                               # (10)
+def on_disconnect(client, userdata, flags, reason_code, properties):                           # (10)
     """Called disconnects from MQTT Broker."""
-    logger.error("Disconnected from MQTT Broker")
+    logger.error("Disconnected from MQTT Broker: " + mqtt.connack_string(reason_code))
 
 
 
 def on_message(client, userdata, msg):                                                         # (11)
     """Callback called when a message is received on a subscribed topic."""
-    logger.debug("Received message for topic {}: {}".format( msg.topic, msg.payload))
+    logger.info("Received message for topic {}: {}".format(msg.topic, msg.payload))
 
-    data = None
-
+                                                                                    # @FIXME - Code reaggange in book
+                                                                                    # @FIXME - #13 removed
     try:
         data = json.loads(msg.payload.decode("UTF-8"))                                         # (12)
+        set_led_brightness(data)                                                               # (XX) @FIXME fn name changed, fix in book
+
     except json.JSONDecodeError as e:
         logger.error("JSON Decode Error: " + msg.payload.decode("UTF-8"))
-
-    if msg.topic == TOPIC:                                                                     # (13)
-        set_led_level(data)                                                                    # (14)
-
-    else:
-        logger.error("Unhandled message topic {} with payload " + str(msg.topic, msg.payload))
 
 
 
@@ -127,12 +124,13 @@ def signal_handler(sig, frame):
     logger.info("You pressed Control + C. Shutting down, please wait...")
 
     client.disconnect() # Graceful disconnection.
-    led.off()
+    pi.set_PWM_dutycycle(LED_GPIO, 0) # 0% = Led off.
     sys.exit(0)
 
 
 
 def init_mqtt():
+    """Initialise MQTT Client and Callbacks"""
     global client
 
     # Our MQTT Client. See PAHO documentation for all configurable options.
@@ -140,6 +138,7 @@ def init_mqtt():
     # for us when we"re offline. You"ll see the "{"session present": 0}" logged when
     # connected.
     client = mqtt.Client(                                                                      # (15)
+        callback_api_version=mqtt.CallbackAPIVersion.VERSION2,
         client_id=CLIENT_ID,
         clean_session=False)
 
@@ -156,8 +155,7 @@ def init_mqtt():
 
 
 
-# Initialise Module
-init_led()
+# Initialise MQTT                           @FIXME comment changed, fix in book
 init_mqtt()
 
 
